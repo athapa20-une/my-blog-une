32851422afdd49986a8b30ff09f2209096f5bdc0
athapa20 <athapa20@myune.edu.au>
2024-09-21 12:37:01 +1000

    Laravel Sanctum api endpoints for posts and detail of post created.

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    // Retrieve a list of all blog posts
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json([
+            'data' => $posts,
+            'message' => 'success'
+        ], 200);
+    }
+
+    // Retrieve details of a single blog post
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'failure'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $post,
+            'message' => 'success'
+        ], 200);    
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 // Route::get('/user', function (Request $request) {
