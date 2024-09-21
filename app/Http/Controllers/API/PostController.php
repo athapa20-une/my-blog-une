10f603c618f342bb9395840a66c6a16614f2be1f
athapa20 <athapa20@myune.edu.au>
2024-09-21 13:56:57 +1000

    User also included in the in the api for blog details.

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -30,7 +31,7 @@ class PostController extends Controller
     public function show($id)
     {
         try {
-            $post = Post::findOrFail($id);
+            $post = Post::with('user')->findOrFail($id);
 
             return response()->json([
                 'data' => $post,
